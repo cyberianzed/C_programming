7b29db44f189ca7355d80a89130554a7ec6a065a
cyberianzed <josekjames5@gmail.com>
2021-05-31 22:53:06 +0530

    Casual commit

--- no.c
+++ no.c
@@ -1,37 +1,11 @@
 #include <stdio.h>
 
+
 int main()
 {
 
-    char 1,2,3,4,5,6,7,8,9,0;
-    
-    char s[40];
 
-    printf("Enter a Word ");
-    scanf("%s", s);
+    printf("Enter a Number ");
 
-    for (j = 0; s[j] != '\0'; j++)
-    {
-        if (s[j] == 'a')
-        {
-            a++;
-        }
-        else if (s[j] == 'e')
-        {
-            e++;
-        }
-        else if (s[j] == 'i')
-        {
-            i++;
-        }
-        else if (s[j] == 'o')
-        {
-            o++;
-        }
-        else if (s[j] == 'u')
-        {
-            u++;
-        }
-        }
-    printf("a = %d\ne = %d\ni = %d\no = %d\nu = %d\n", a, e, i, o, u);
+    scanf("%lld", &num);
 }
---
+++ switch.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int num = 2;
+
+    switch (num)
+    {
+    case 1:
+        printf("One\n");
+        break;
+    case 2:
+        printf("Two\n");
+        break;
+    case 3:
+        printf("Three\n");
+        break;
+
+    default:
+        break;
+    }
+}
