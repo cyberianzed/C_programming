071ce7e370836e3c1923a1ddde14afa60f0d91a3
Jose k James <josekjames5@gmail.com>
2021-06-06 23:59:21 +0530

    casual

--- .vscode/quadrent.c
+++ .vscode/quadrent.c
@@ -1,7 +1,9 @@
-/*Write a C program to accept a coordinate point in a XY coordinate system and determine in which quadrant the coordinate point lies. Go to the editor
+/*
+Write a C program to accept a coordinate point in a XY coordinate system and determine in which quadrant the coordinate point lies. Go to the editor
 Test Data : 7 9
 Expected Output :
-The coordinate point (7,9) lies in the First quadrant. */
+The coordinate point (7,9) lies in the First quadrant. 
+*/
 
 #include <stdio.h>
 void main()
@@ -23,4 +25,10 @@ void main()
         else
 
             printf("In 2nd Quadrent");
+    
+    if (fn==0 && sn==0)
+    {
+        printf("At Origin");
+    }
+    
 }
