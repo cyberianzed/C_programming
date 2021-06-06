d1004eb69bcf8758479871f68d5b53d755b88793
Jose k James <josekjames5@gmail.com>
2021-06-06 23:46:49 +0530

    casual

--- .vscode/quadrent.c
+++ .vscode/quadrent.c
@@ -6,7 +6,21 @@ The coordinate point (7,9) lies in the First quadrant. */
 #include <stdio.h>
 void main()
 {
-    int co;
-    printf("Enter coordinate :");
-    scanf("%d", &co);
+    int co, fn, sn;
+    printf("Enter coordinate :\n");
+    scanf("%d %d", &fn, &sn);
+
+    if (fn > 0)
+        if (sn > 0)
+            printf("In 1st Quadrent");
+        else
+
+            printf("In 4th Quadrent");
+
+    if (fn < 0)
+        if (sn < 0)
+            printf("In 3rd Quadrent");
+        else
+
+            printf("In 2nd Quadrent");
 }
