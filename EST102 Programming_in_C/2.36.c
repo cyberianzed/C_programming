c754f258e2a172871dc9df22f8de21274e19b074
Jose K James <josekjames5@gmail.com>
2021-09-03 23:16:51 +0530

    2.36

---
+++ EST102 Programming_in_C/2.36.c
@@ -0,0 +1,44 @@
+/******************************************************************
+ * File         :   2.38
+ * Description  :   Calender
+ * Author       :   Jose K James
+ * Version      :   0.12
+ * Date         :   08/08/2021
+
+2.38
+******************************************************************/
+#include <stdio.h>
+
+void main()
+{
+    int i, j, k, day, dt = 1;
+
+    printf("C Program to Print a Calendar With Start With Input Day\n\n");
+
+    printf("Enter Total Numbers of Days in a Month : ");
+    scanf("%d", &day);
+
+    printf("\n\nEnter First Day Start From <0-Mon....5-Sat & 6-Sun> End With Sunday : ");
+    scanf("%d", &k);
+
+    printf("\nMon \tTue \tWed \tThu \tFri \tSat \tSun\n\n");
+    printf(" _________________________________________________\n\n");
+
+    for (j = k; j > 0; j--)
+    {
+        printf("\t");
+    }
+
+    while (dt <= day)
+    {
+        if (k != 0)
+        {
+            if (k % 7 == 0)
+                printf("\n");
+        }
+
+        printf("%d\t", dt);
+        dt++;
+        k++;
+    }
+}
